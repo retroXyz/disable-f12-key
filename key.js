223905386aee7aea0326ee61a697a161811197c5
retr0_ <82467575+retroXyz@users.noreply.github.com>
2021-07-16 17:07:00 +0300

    Create key.js

---
+++ key.js
@@ -0,0 +1,25 @@
+// key.js
+// f12.keyDisable.
+
+// # $start?
+
+function f12consoleKey(){ // .start?
+
+    // EventListenersAdding.
+     // .start?
+
+    window.addEventListener('keydown', event=>{ // .start?
+
+        if(event.keyCode == 123 && event.target == document.body){ // .start?
+            event.preventDefault();
+        }; // .end?
+
+    }); // .end?
+
+    // EventListenersAdding.
+     // .end?
+
+}; // .end?
+f12consoleKey(); // .end?
+
+// # $end?
